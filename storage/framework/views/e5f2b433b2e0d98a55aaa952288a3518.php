0dfc5c6e4ffdb6079b4d59ee085aa22e4d1fc9ed
Agung Kurniawan <155374842+Agung2828@users.noreply.github.com>
2024-05-29 16:36:56 +0700

    Add files via upload

---
+++ storage/framework/views/e5f2b433b2e0d98a55aaa952288a3518.php
@@ -0,0 +1,135 @@
+
+
+<?php if(Auth::user()->role === 'admin'): ?>
+    <?php $__env->startSection('navitem'); ?>
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
+
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item">
+            <a class="nav-link" href="/admin">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Prosedur</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo e(route('usercontrol')); ?>">
+                <i class="fas fa-fw fa-users"></i>
+                <span>User Control</span></a>
+        </li>
+
+        <!-- Nav Item - Tables -->
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo e(route('datapengguna')); ?>">
+                <i class="fas fa-fw fa-file-alt"></i>
+                <span>Data Pengajuan</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo e(route('dataaplikasi')); ?>">
+                <i class="fas fa-fw fa-chart-area"></i>
+                <span>Data Aplikasi</span></a>
+        </li>
+        <!-- Divider -->
+        <hr class="sidebar-divider d-none d-md-block">
+
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+            <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
+    <?php $__env->stopSection(); ?>
+<?php endif; ?>
+<?php $__env->startSection('main'); ?>
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <h1 class="h3 mb-2 text-gray-800">Data Pengajuan</h1> <!-- DataTales Example -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <a href="/dataaplikasi/brktambah" class="btn-sm btn-primary text-decoration-none">Tambah data</a>
+
+                <?php if($errors->any()): ?>
+                    <div class="alert alert-danger">
+                        <ul>
+                            <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                <li><?php echo e($item); ?></li>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                        </ul>
+                    </div>
+                <?php endif; ?>
+                <?php if(Session::has('success')): ?>
+                    <script>
+                        document.addEventListener('DOMContentLoaded', function() {
+                            Swal.fire(
+                                'Sukses',
+                                '<?php echo e(Session::get('success')); ?>',
+                                'success'
+                            );
+                        });
+                    </script>
+                <?php endif; ?>
+
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Nama Aplikasi</th>
+                                <th>action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $__currentLoopData = $data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                <tr>
+                                    <td><?php echo e($item->namaaplikasi); ?></td>
+                                    <td>
+                                        <a href="/dataaplikasi/brkedit/<?php echo e($item->id); ?>"
+                                            class="btn btn-sm btn-warning">Edit</a>
+                                        <form onsubmit="return confirmDelete(event)"
+                                            action="/dataaplikasi/brkhapus/<?php echo e($item->id); ?>" method="POST"
+                                            class="d-inline">
+                                            <?php echo csrf_field(); ?>
+                                            <button type="submit" class="btn btn-sm btn-danger">Hapus</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </div>
+<?php $__env->stopSection(); ?>
+<?php $__env->startPush('js'); ?>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script>
+        function confirmDelete(event) {
+            event.preventDefault(); // Menghentikan form dari pengiriman langsung
+
+            Swal.fire({
+                title: 'Yakin Hapus Data?',
+                text: "Data yang dihapus tidak dapat dikembalikan!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#d33',
+                cancelButtonColor: '#3085d6',
+                confirmButtonText: 'Hapus',
+                cancelButtonText: 'Batal'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    event.target.submit(); // Melanjutkan pengiriman form
+                }
+            });
+        }
+    </script>
+<?php $__env->stopPush(); ?>
+
+<?php echo $__env->make('halaman_dashboard.index', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel10\project-laravel10\resources\views/data_aplikasi/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/f8473e42690e829720b46abde3db9830.php
@@ -0,0 +1,198 @@
+
+<?php if(Auth::user()->role === 'user'): ?>
+    <?php $__env->startSection('navitem'); ?>
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
+
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item active">
+            <a class="nav-link" href="/user">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo e(route('datapelanggan')); ?>">
+                <i class="fas fa-fw fa-file-alt"></i>
+                <span>Pengajuan</span></a>
+        </li>
+        <!-- Divider -->
+        <hr class="sidebar-divider d-none d-md-block">
+
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+            <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
+    <?php $__env->stopSection(); ?>
+<?php elseif(Auth::user()->role === 'user'): ?>
+    <?php $__env->startSection('navitem'); ?>
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
+
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item active">
+            <a class="nav-link" href="/user">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                aria-expanded="true" aria-controls="collapseTwo">
+                <i class="fas fa-fw fa-cog"></i>
+                <span>Components</span>
+            </a>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Custom Components:</h6>
+                    <a class="collapse-item" href="buttons.html">Buttons</a>
+                    <a class="collapse-item" href="cards.html">Cards</a>
+                </div>
+            </div>
+        </li>
+
+        <!-- Nav Item - Utilities Collapse Menu -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+                aria-expanded="true" aria-controls="collapseUtilities">
+                <i class="fas fa-fw fa-wrench"></i>
+                <span>Utilities</span>
+            </a>
+            <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Custom Utilities:</h6>
+                    <a class="collapse-item" href="utilities-color.html">Colors</a>
+                    <a class="collapse-item" href="utilities-border.html">Borders</a>
+                    <a class="collapse-item" href="utilities-animation.html">Animations
+                    <?php $__env->stopSection(); ?>
+<?php endif; ?>
+<?php $__env->startSection('main'); ?>
+    <div class="col-12 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title mb-4">Tambah data BRK</h4>
+                <?php if($errors->any()): ?>
+                    <div class="alert alert-danger">
+                        <ul>
+                            <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                <li><?php echo e($item); ?></li>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                        </ul>
+                    </div>
+                <?php endif; ?>
+                <form class="forms-sample" method="POST" action="/datapelanggan/brktambah" enctype="multipart/form-data">
+                    <?php echo csrf_field(); ?>
+                    <div class="form-group">
+                        <div class="form-group">
+                            <label>Jenis Layanan</label><br>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="update_data" name="jenis_layanan[]"
+                                    value="update data">
+                                <label class="form-check-label" for="update data">update data</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="perbaikan_aplikasi"
+                                    name="jenis_layanan[]" value="Perbaikan Aplikasi">
+                                <label class="form-check-label" for="perbaikan_aplikasi">Perbaikan Aplikasi</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="aplikasi_baru" name="jenis_layanan[]"
+                                    value="Aplikasi_baru">
+                                <label class="form-check-label" for="aplikasi_baru">Aplikasi baru</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="migrasi" name="jenis_layanan[]"
+                                    value="Migrasi">
+                                <label class="form-check-label" for="migrasi">Migrasi</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="cleansing" name="jenis_layanan[]"
+                                    value="cleansing">
+                                <label class="form-check-label" for="cleansing">cleansing</label>
+                            </div>
+                        </div>
+
+                        <label for="nama">Nama</label>
+                        <input type="text" class="form-control" id="nama" name="nama" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="nama_aplikasi">Nama Aplikasi</label>
+                        <select class="form-control" id="namaaplikasi" name="nama_aplikasi" required>
+                            <option value="" disabled selected style="color: rgba(0,0,0,0.5);">Pilih Nama Aplikasi
+                            </option>
+                            <option value="AMARIS">AMARIS</option>
+                            <option value="ANTASENA">ANTASENA</option>
+                            <option value="API">FMIS</option>
+                            <option value="Bank Vision">Bank Vision</option>
+                            <option value="BI FAST">BI FAST</option>
+                            <option value="BI FAST REPORT">BI FAST REPORT</option>
+                            <option value="BP Jamsostek<">BP Jamsostek</option>
+                            <option value="BRK LINK">BRK LINK</option>
+                            <option value="CMS">CMS</option>
+                            <option value="DMS">DMS</option>
+                            <option value="EFOS">EFOS</option>
+                            <option value="FLPP">FLPP</option>
+                            <option value="GO AML">GO AML</option>
+                            <option value="HIRIS">HIRIS</option>
+                            <option value="IKP">IKP</option>
+                            <option value="Kasda Online">Kasda Online</option>
+                            <option value="KUR">KUR</option>
+                            <option value="KUR iB">KUR iB</option>
+                            <option value="MlS">MlS</option>
+                            <option value="Naskah Dinas">Naskah Dinas</option>
+                            <option value="Papan Nisbah & KURS">Papan Nisbah & KURS</option>
+                            <option value="Payment System">Payment System</option>
+                            <option value="Payroll Online">Payroll Online</option>
+                            <option value="PBB Kolektif">PBB Kolektif</option>
+                            <option value="PRF">PRF</option>
+                            <option value="Priority Banking">Priority Banking</option>
+                            <option value="Program Budaya">Program Budaya</option>
+                            <option value="QACA">QACA</option>
+                            <option value="QRIS">QRIS</option>
+                            <option value="Rekon ATM Bersama">Rekon ATM Bersama</option>
+                            <option value="Rekon Debit Issuer">Rekon Debit Issuer</option>
+                            <option value="Rekon IBFT">Rekon IBFT</option>
+                            <option value="SDB">SDB</option>
+                            <option value="SIAP">SIAP</option>
+                            <option value="SIKP">SIKP</option>
+                            <option value="SIDP">SIDP</option>
+                            <option value="SIPENA">SIPENA</option>
+                            <option value="SIPERU">SIPERU</option>
+                            <option value="SKPD ONLINE">SKPD ONLINE</option>
+                            <option value="SPAK">SPAK</option>
+                            <option value="SPAN">SPAN</option>
+                            <option value="Web CIF CS">Web CIF CS</option>
+                            <option value="Website BRKS">Website BRKS</option>
+                            <option value="Whistleblowing">Whistleblowing</option>
+                            <!-- tambahkan opsi lain sesuai kebutuhan -->
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="IpApp">IpApp</label>
+                        <input type="text" class="form-control" id="IpApp" name="IpApp" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="platform">platform</label>
+                        <input type="text" class="form-control" id="platform" name="platform" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="Jenis">Jenis perbaikan/update</label>
+                        <input type="text" class="form-control" id="Jenis" name="jenis" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="tanggalpermohonan">Tanggal Permohonan</label>
+                        <input type="datetime-local" class="form-control" id="tanggalpermohonan"
+                            name="tanggalpermohonan" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary me-2">Tambah</button>
+                    <a href="/datapelanggan" class="btn btn-light">Kembali</a>
+                </form>
+            </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('halaman_dashboard.index', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel10\project-laravel10\resources\views/data_pelanggan/tambah.blade.php ENDPATH**/ ?>
